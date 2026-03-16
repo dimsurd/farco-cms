abdb36df74b254305e59491b9990c6bc1c0a019f
dimassuryadilana <dimassuryadilana@gmail.com>
2026-03-17 00:18:04 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::updateOrCreate([
+            'email' => env('SUPER_ADMIN_EMAIL', 'admin@farco.local'),
+        ], [
+            'name' => env('SUPER_ADMIN_NAME', 'Super Admin'),
+            'password' => Hash::make(env('SUPER_ADMIN_PASSWORD', 'ChangeMe123!')),
+            'role' => 'super_admin',
         ]);
     }
 }
